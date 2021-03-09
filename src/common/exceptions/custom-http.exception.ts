e63d295f059d1b611fc7bab1c7c4a5a19f0e3855
Nikita Dyshaev <dyshaev.working@gmail.com>
2021-03-09 21:15:33 +0300

    update http exception

--- src/common/exceptions/custom-http.exception.ts
+++ src/common/exceptions/custom-http.exception.ts
@@ -1,10 +1,10 @@
 import { HttpException, HttpStatus } from '@nestjs/common';
 
 export class CustomHttpException extends HttpException {
-  constructor(error: any, message, status: number) {
-    super(
-      Object.assign(error, error.message || message),
-      status || HttpStatus.INTERNAL_SERVER_ERROR,
-    );
+  constructor(error: any, errorMessage: string, errorStatus?: number) {
+    const message = error.message || errorMessage;
+    const status = errorStatus || HttpStatus.INTERNAL_SERVER_ERROR;
+
+    super(Object.assign(error, message), status);
   }
 }
